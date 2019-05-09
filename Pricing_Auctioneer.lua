8f733a50f446302ca9ef0fd697ae6313da76e812
Mike Battersby <mib@unimelb.edu.au>
2019-05-09 13:05:57 +1000

    Add copyright and licensing notices

--- Pricing_Auctioneer.lua
+++ Pricing_Auctioneer.lua
@@ -1,3 +1,22 @@
+--[[----------------------------------------------------------------------------
+
+    Copyright 2019 Mike Battersby
+
+    This file is part of TradeSkillPrice.
+
+    TradeSkillPrice is free software; you can redistribute it or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation, version 2, 1991.
+
+    TradeSkillPrice is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    See the file LICENSE.txt for details.
+
+----------------------------------------------------------------------------]]--
+
 local function Value(itemID)
     local price = AucAdvanced.API.GetMarketValue(itemID)
     if price then
--- Pricing_Vendor.lua
+++ Pricing_Vendor.lua
@@ -1,3 +1,22 @@
+--[[----------------------------------------------------------------------------
+
+    Copyright 2019 Mike Battersby
+
+    This file is part of TradeSkillPrice.
+
+    TradeSkillPrice is free software; you can redistribute it or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation, version 2, 1991.
+
+    TradeSkillPrice is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    See the file LICENSE.txt for details.
+
+----------------------------------------------------------------------------]]--
+
 local function GetVendorCost(itemID)
     -- Unbound Tradeskill - Other items are assumed to be buyable
     local sellPrice, classID, subID, bindType = select(11, GetItemInfo(itemID))
