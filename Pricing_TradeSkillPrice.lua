a0fc3356f665d6ede0d934b9a67ae9be3a146cdc
Mike "Xodiv" Battersby <mib@post.com>
2019-05-15 00:10:15 +1000

    I think this is a working getall AH scan

--- Pricing_TradeSkillPrice.lua
+++ Pricing_TradeSkillPrice.lua
@@ -18,12 +18,35 @@
 ----------------------------------------------------------------------------]]--
 
 local abortScan
+local unregisteredFrames = {}
+
+local scanner = CreateFrame('Frame')
+
+function LockoutOthers()
+    local frames = { GetFramesRegisteredForEvent('AUCTION_ITEM_LIST_UPDATE') }
+    for _,f in ipairs(frames) do
+        if f ~= scanner then
+            table.insert(unregisteredFrames, f)
+            f:UnregisterEvent('AUCTION_ITEM_LIST_UPDATE')
+        end
+    end
+end
+
+function UnlockOthers()
+    QueryAuctionItems('xyzzy', nil, nil, 0, nil, nil, false, false, nil)
+    for _,f in ipairs(unregisteredFrames) do
+        f:RegisterEvent('AUCTION_ITEM_LIST_UPDATE')
+    end
+    table.wipe(unregisteredFrames)
+end
 
 local function StartScan(now, size)
 
     local name, texture, count, quality, canUse, level, levelColHeader, minBid,
         minIncrement, buyoutPrice, bidAmount, highBidder, bidderFullName, owner,
         ownerFullName, saleStatus, itemID, hasAllInfo
+
+    local data = TSP.db.auctionData
 
     for i = 1, size do
         if abortScan then
@@ -51,14 +74,22 @@ local function StartScan(now, size)
         hasAllInfo      -- [18]
             = GetAuctionItemInfo('list', i)
 
+        -- Saving the dumbest of dumb data, just the lowest price
+        -- we've seen the most recently.
+
         if buyoutPrice and buyoutPrice > 0 then
-            TSP.auctionData[itemID] = TSP.auctionData[itemID] or  {}
-            table.insert(TSP.auctionData[itemID], { buyoutPrice/count, now })
+            local costPer = buyoutPrice / count
+            if not data[itemID]
+               or data[itemID].when ~= now
+               or costPer < data[itemID].price then
+                data[itemID] = { price=costPer, when=now }
+            else
+                data[itemID].when = now
+            end
         end
+
         if i % 1000 == 0 then
-            print(i)
-        end
-        if i % 25 == 0 then
+            TSP:ChatMessage("Processing auction data: %d/%d", i, size)
             coroutine.yield()
         end
     end
@@ -66,7 +97,7 @@ end
 
 local function OnUpdate(self, elapsed)
     self.totalElapsed = (self.totalElapsed or 0) + elapsed
-    if self.totalElapsed < 0.1 then
+    if self.totalElapsed < 0.01 then
         return
     else
         self.totalElapsed = 0
@@ -76,10 +107,12 @@ local function OnUpdate(self, elapsed)
         self:SetScript('OnUpdate', nil)
         self.thread = nil
         self.totalElapsed = nil
+        UnlockOthers()
     else
+        TSP:ChatMessage('Scheduling thread')
         local t, e = coroutine.resume(self.thread)
         if t == false then
-            print(e)
+            TSP:ChatMessage(e)
         end
     end
 end
@@ -88,27 +121,27 @@ local function AuctionItemListUpdate(self)
     local batchSize, totalItems = GetNumAuctionItems('list')
 
     if batchSize ~= totalItems then
+        TSP:ChatMessage('Non-getall scan found, ignoring')
         -- we only support getall scans
         return
     end
 
-    -- You can't get another set of getall data within 15 minutes so
-    -- all the rest of the events are refinements of the getall
+    -- You can't get another set of getall data within 15 minutes so all the
+    -- rest of the events are refinements of the getall. Or, something did
+    -- another search, in which case we're boned.
+
     if self.thread then
         return
     end
 
-    TSP.auctionData = TSP.auctionData or {}
+    TSP.db.auctionData = TSP.db.auctionData or {}
 
     local now = time()
 
+    TSP:ChatMessage('Creating thread')
     self.thread = coroutine.create(function () StartScan(now, batchSize) end)
     self:SetScript('OnUpdate', OnUpdate)
 end
-
--- it doesn't really matter that much if this is a bit slow to query as long
--- as it's not too slow. Because the getall scan returns so much data we really
--- need to optimize for dealing with that.
 
 local function OnEvent(self, event, ...)
     if event == 'AUCTION_HOUSE_SHOW' then
@@ -121,14 +154,39 @@ local function OnEvent(self, event, ...)
     end
 end
 
+local function GetMinPrice(itemID)
+    if TSP.db.auctionData and TSP.db.auctionData[itemID] then
+        return TSP.db.auctionData[itemID].price, "a"
+    end
+end
+
 function TSP:ScanAH()
     local canQuery, canQueryAll = CanSendAuctionQuery()
     if canQueryAll then
+        LockoutOthers()
         QueryAuctionItems('', nil, nil, 0, nil, nil, true, false, nil)
     end
 end
 
-local scanner = CreateFrame('Frame')
-scanner:RegisterEvent('AUCTION_HOUSE_SHOW')
-scanner:RegisterEvent('AUCTION_HOUSE_CLOSED')
-scanner:SetScript('OnEvent', OnEvent)
+
+-- This is not a good test
+
+if #TSP.valueFunctions == 1 then
+    scanner:RegisterEvent('AUCTION_HOUSE_SHOW')
+    scanner:RegisterEvent('AUCTION_HOUSE_CLOSED')
+    scanner:SetScript('OnEvent', OnEvent)
+
+    table.insert(TSP.valueFunctions,
+                {
+                    ['name'] = 'TradeSkillPrice',
+                    ['func'] =  GetMinPrice,
+                })
+    table.insert(TSP.costFunctions,
+                {
+                    ['name'] = 'TradeSkillPrice',
+                    ['func'] =  GetMinPrice,
+                })
+else
+    -- Don't keep our old possible stale data around
+    TSP.db.auctionData = nil
+end
