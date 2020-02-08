a8678fb5cb777eb16c6bab9244724f8fdfe5d3b9
Mike "Xodiv" Battersby <mib@post.com>
2020-02-09 08:51:56 +1100

    Keep AH data per realm

--- Pricing_Builtin.lua
+++ Pricing_Builtin.lua
@@ -32,7 +32,7 @@ local AH_ITEM_EVENTS = {
 
 -- local abortScan, lastGetAllTime = nil, 0
 
-local AuctionHouseScanner = CreateFrame('Frame', 'TradeSkillPriceAHScanner')
+local AHScanner = CreateFrame('Frame', 'TradeSkillPriceAHScanner')
 
 local function CreateScanButton()
     if AuctionHouseFrame and not TradeSkillPriceAHScanButton then
@@ -45,144 +45,20 @@ local function CreateScanButton()
     end
 end
 
---[[
-local function CreateLockoutFrame()
-
-    local f = CreateFrame('Frame', nil, AuctionHouseFrame)
-    f:EnableMouse(true)
-    f:SetFrameStrata('HIGH')
-    f:SetAllPoints()
-
-    f.texture = f:CreateTexture(nil, 'ARTWORK')
-    f.texture:SetAllPoints()
-    f.texture:SetColorTexture(0, 0.5, 0, 0.5)
-
-    f.message = f:CreateFontString(nil, 'OVERLAY')
-    f.message:SetPoint('CENTER', f, 'CENTER', 0, 48)
-    f.message:SetFontObject(GameFontHighlightHuge)
-    f.message:SetText('Auction House scan in progress, please wait.')
-
-    f.progress = f:CreateFontString(nil, 'OVERLAY')
-    f.progress:SetPoint('CENTER', f, 'CENTER', 0, 0)
-    f.progress:SetFontObject(GameFontNormalHuge)
-
-    f.cancel = CreateFrame('Button', nil, f, 'MagicButtonTemplate')
-    f.cancel:SetPoint('CENTER', f, 'CENTER', 0, -48)
-    f.cancel:SetText(CANCEL)
-    f.cancel:SetScript('OnClick', function () abortScan = true end)
-
-    return f
-end
-]]
-
 local function UpdateItemPrice(itemID, price, count, when)
     local data = TradeSkillPrice.db.auctionData
-    if not data[itemID] then
-        data[itemID] = {
+    if not AHScanner.data[itemID] then
+        AHScanner.data[itemID] = {
                 price = price,
                 count = count,
                 when = when
             }
     elseif data[itemID].when < when or price < data[itemID].price then
-        data[itemID].price = price
-        data[itemID].count = count
-        data[itemID].when = when
+        AHScanner.data[itemID].price = price
+        AHScanner.data[itemID].count = count
+        AHScanner.data[itemID].when = when
     end
 end
-
---[[
-local function StartScan(size)
-
-    local now = time()
-
-    local name, texture, count, quality, canUse, level, levelColHeader, minBid,
-        minIncrement, buyoutPrice, bidAmount, highBidder, bidderFullName, owner,
-        ownerFullName, saleStatus, itemID, hasAllInfo
-
-    local data = TradeSkillPrice.db.auctionData
-
-    local nConsecutiveFailures = 0
-
-    for i = 1, size do
-        if abortScan then
-            TradeSkillPrice:ChatMessage("Scan aborted")
-            abortScan = nil
-            return
-        end
-
-        name,           -- [1]
-        texture,        -- [2]
-        count,          -- [3]
-        quality,        -- [4]
-        canUse,         -- [5]
-        level,          -- [6]
-        levelColHeader, -- [7]
-        minBid,         -- [8]
-        minIncrement,   -- [9]
-        buyoutPrice,    -- [10]
-        bidAmount,      -- [11]
-        highBidder,     -- [12]
-        bidderFullName, -- [13]
-        owner,          -- [14]
-        ownerFullName,  -- [15]
-        saleStatus,     -- [16]
-        itemID,         -- [17]
-        hasAllInfo      -- [18]
-            = C_AuctionHouse.GetReplicateItemInfo(i-1)
-
-        -- Saving the dumbest of dumb data, just the lowest price
-        -- we've seen the most recently.
-
-        if buyoutPrice and buyoutPrice > 0 then
-            local costPer = buyoutPrice / count
-            if not data[itemID]
-               or data[itemID].when ~= now
-               or costPer < data[itemID].price then
-                data[itemID] = { price=costPer, when=now }
-            else
-                data[itemID].when = now
-            end
-            nConsecutiveFailures = 0
-        else
-            nConsecutiveFailures = nConsecutiveFailures + 1
-            if nConsecutiveFailures > 100 then
-                abortScan = true
-            end
-        end
-
-        -- This modulus should be at least NUM_AUCTION_ITEMS_PER_PAGE (50)
-
-        if i % 1000 == 0 then
-            coroutine.yield()
-        end
-    end
-    TradeSkillPrice:ChatMessage('Scan completed, %d items scanned.', size)
-end
-]]
-
---[[
-local function OnUpdate(self, elapsed)
-    self.totalElapsed = (self.totalElapsed or 0) + elapsed
-    if self.totalElapsed < 0.01 then
-        return
-    else
-        self.totalElapsed = 0
-    end
-
-    if not self.thread or coroutine.status(self.thread) == 'dead' then
-        self:SetScript('OnUpdate', nil)
-        self.thread = nil
-        self.totalElapsed = nil
-        QueryAuctionItems('x_y_z_z_y')
-        C_Timer.After(0.5, UnlockBlizzard)
-    else
-        local t, e = coroutine.resume(self.thread)
-        if t == false then
-            TradeSkillPrice:ChatMessage(e)
-        end
-    end
-end
-]]
 
 local function ProcessReplicateItemList(self)
     local now = time()
@@ -217,8 +93,8 @@ local function ProcessBrowseResults(self, browseResults)
 end
 
 local function GetMinPrice(itemID, count)
-    if TradeSkillPrice.db.auctionData and TradeSkillPrice.db.auctionData[itemID] then
-        return TradeSkillPrice.db.auctionData[itemID].price * count, "a"
+    if AHScanner.data and AHScanner.data[itemID] then
+        return AHScanner.data[itemID].price * count, "a"
     end
 end
 
@@ -226,8 +102,8 @@ local function TooltipAddPrice(ttFrame, link, count)
     if not link then return end
     count = count or 1
     local id = GetItemInfoFromHyperlink(link)
-    if id and TradeSkillPrice.db.auctionData[id] then
-        local copper = TradeSkillPrice.db.auctionData[id].price
+    if id and AHScanner.data[id] then
+        local copper = AHScanner.data[id].price
         local price = GetMoneyString(copper, true)
         local text = format('|cff80d060Auction :|r |cffffffff%s', price)
         ttFrame:AddLine(text)
@@ -246,22 +122,30 @@ end
 
 local function Init(self)
 
-    -- This is not a good test
-    if #TradeSkillPrice.valueFunctions == 1 then
-        TradeSkillPrice.db.auctionData = TradeSkillPrice.db.auctionData or {}
+    -- This is not a good test. Assume if we only have vendor we shoud load
+    if #TradeSkillPrice.valueFunctions > 1 then
+        TradeSkillPrice.db.auctionData = nil
+        return
+    end
 
-        FrameUtil.RegisterFrameForEvents(self, AH_SHOWHIDE_EVENTS)
+    local r = GetRealmName()
+    TradeSkillPrice.db.auctionData = TradeSkillPrice.db.auctionData or {}
+    TradeSkillPrice.db.auctionData[r] = TradeSkillPrice.db.auctionData[r] or {}
 
-        table.insert(TradeSkillPrice.valueFunctions,
-                    {
-                        ['name'] = 'TradeSkillPrice',
-                        ['func'] =  GetMinPrice,
-                    })
-        table.insert(TradeSkillPrice.costFunctions,
-                    {
-                        ['name'] = 'TradeSkillPrice',
-                        ['func'] =  GetMinPrice,
-                    })
+    self.data = TradeSkillPrice.db.auctionData[r]
+
+    FrameUtil.RegisterFrameForEvents(self, AH_SHOWHIDE_EVENTS)
+
+    table.insert(TradeSkillPrice.valueFunctions,
+                {
+                    ['name'] = 'TradeSkillPrice',
+                    ['func'] =  GetMinPrice,
+                })
+    table.insert(TradeSkillPrice.costFunctions,
+                {
+                    ['name'] = 'TradeSkillPrice',
+                    ['func'] =  GetMinPrice,
+                })
 
     hooksecurefunc(GameTooltip, 'SetBagItem',
         function (ttFrame, bag, slot)
@@ -298,10 +182,6 @@ local function Init(self)
             local _, _, count = C_TradeSkillUI.GetRecipeReagentInfo(id, index)
             TooltipAddPrice(ttFrame, link, count)
         end)
-    else
-        TradeSkillPrice.db.auctionData = nil
-    end
-
 end
 
 local function OnEvent(self, event, arg1, ...)
@@ -329,5 +209,5 @@ local function OnEvent(self, event, arg1, ...)
     end
 end
 
-AuctionHouseScanner:RegisterEvent('ADDON_LOADED')
-AuctionHouseScanner:SetScript('OnEvent', OnEvent)
+AHScanner:RegisterEvent('ADDON_LOADED')
+AHScanner:SetScript('OnEvent', OnEvent)
